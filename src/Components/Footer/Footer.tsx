6d0cfcc47765ee62f81753d3757646036256a525
anastasiia1210 <anastasiia.lysa@ukma.edu.ua>
2023-05-30 19:14:31 +0300

    create home page

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import "./footer.css";
 import { Icon } from '@iconify/react';
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
     const company = "Little fox";
-    const phone = "+380976809114"
-    const email = "shelter_kyiv@gmail.com"
+    const phone = "+380976809114";
+    const email = "shelter_kyiv@gmail.com";
     const year = new Date().getFullYear();
 
     return (
         <footer>
             <div className="footer-content">
 
-                <div className="contacts">
+                <div className="footer-content__left">
                     <p>Телефонуйте: {phone}</p>
                     <p>Email: {email}</p>
                     <p>Графік роботи: 10:00-20:00 пн-сб</p>
                     <p className="copyright">&copy; {year} {company}. All rights reserved.</p>
                 </div>
-                <div>
+                <div className="footer-content__right">
                     <div className="icons">
                         <a href="https://www.instagram.com/zelenskiy_official/">
                             <Icon icon="mdi:instagram" />
@@ -29,6 +30,9 @@ const Footer: React.FC = () => {
                         <a href="https://www.facebook.com/zelenskiy.official/">
                             <Icon icon="mdi:facebook" />
                         </a>
+                    </div>
+                    <div className="sign-in-wrapper">
+                        <Link className="sign-in" to="/donate">Вхід</Link>
                     </div>
                 </div>
             </div>
